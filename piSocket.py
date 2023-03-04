130ff37c9fd2a9c3002f71f6c50901920d43a719
LumberUniverse <33379217+LumberUniverse@users.noreply.github.com>
2023-03-04 10:28:46 +0200

    IP change
    Changing server ip

--- piSocket.py
+++ piSocket.py
@@ -99,7 +99,7 @@ GPIO.output(relay, GPIO.LOW)
 connected = False
 while not connected:
     try:
-        sio.connect("http://192.168.0.105:3001")
+        sio.connect("http://192.168.110.216:3001")
         print("Socket established")
         connected = True
     except Exception as ex:
