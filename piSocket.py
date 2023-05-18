9e40137f24e6e8f180af9795b10334a4c4f26dc7
Tristan <33379217+LumberUniverse@users.noreply.github.com>
2023-05-18 20:24:36 +0200

    adding the offline ringing

--- piSocket.py
+++ piSocket.py
@@ -3,12 +3,30 @@ import RPi.GPIO as GPIO
 import time
 import datetime
 import multiprocessing
+import os
 
 relay = 4
 
-symbol_times = None
+connected = False
+next_ring = None
+last_date = None
+today_times = []
+patterns = {}
+isLoop = {
+  'normal': False,
+  'riot': True,
+  'fire': True,
+  'bomb': True,
+}
+speeds = {
+    ' ': 0.5,
+    '.': 0.5,
+    '-': 1,
+    '_': 2,
+}
 
 GPIO.setwarnings(False)
+GPIO.cleanup()
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(relay, GPIO.OUT)
 
@@ -19,8 +37,23 @@ def power_relay(seconds):
     GPIO.output(relay, GPIO.HIGH)
     time.sleep(seconds)
 
+    
+# Global variable to keep track of the current process
+current_process = None
+currently_ringing = None
+
+def clear_ringing_thread():
+    global current_process
+
+    if current_process is not None and current_process.is_alive():
+        os.kill(current_process.pid, 9)
+        current_process.join()
+
+    current_process = None
+
 # Function to apply the pattern to the relay
 def apply_pattern(pattern, isLoop):
+    global speeds, relay
     # Iterate through each character in the pattern
     times = 0
 
@@ -30,10 +63,10 @@ def apply_pattern(pattern, isLoop):
         for c in pattern:
             if c == ' ':
                 GPIO.output(relay, GPIO.LOW)
-                time.sleep(symbol_times.get(c, 0.5))
+                time.sleep(speeds.get(c, 0.5))
                 pass
             else:
-                _tempTime = symbol_times.get(c, 0.5)
+                _tempTime = speeds.get(c, 0.5)
                 power_relay(_tempTime / 2)
 
                 if c == '.':
@@ -41,51 +74,97 @@ def apply_pattern(pattern, isLoop):
                     
                 time.sleep(_tempTime / 2)
 
-# Global variable to keep track of the current process
-current_process = None
-currently_ringing = None
+    clear_ringing_thread()
 
 # Function to start a new apply_pattern process
 def start_process(pattern, isLoop):
-    global current_process
+    global current_process, next_ring
+    next_ring = None
     # If a process is already running, terminate it
     if current_process is not None:
-        current_process.terminate()
-        current_process.join()
+        clear_ringing_thread()
     # Start the new apply_pattern process
     current_process = multiprocessing.Process(target=apply_pattern, args=(pattern, isLoop,))
     current_process.start()
 
+_offline_ringer = None
+
+def offline_ringer():
+    global connected, isLoop, patterns, next_ring, last_date, today_times
+
+    while (not connected) and (last_date is not None) and (today_times is not None):
+        if (last_date == datetime.date.today().strftime('%Y%m%d')):
+            seconds = (datetime.datetime.now() - datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds()
+            old_last_rung = next_ring
+
+            for period in today_times:
+                for bell_time in [period.get("start", 0) * 60, period.get("end", 0) * 60]:
+                    if (seconds < bell_time and ((next_ring is None) or bell_time < next_ring)):
+                        next_ring = bell_time
+
+            if (next_ring is not None and next_ring > today_times[-1].get("end", 0) * 60):
+                next_ring = None
+
+            if (next_ring != old_last_rung):
+                print("Next ring:", next_ring)
+
+            if (next_ring is not None and seconds >= next_ring):
+                start_process(patterns.get("normal", " "), isLoop.get("normal", False))
+
+        time.sleep(0.5)
+
+def start_offline_ringer():
+    global _offline_ringer
+
+    _offline_ringer = multiprocessing.Process(target=offline_ringer)
+    _offline_ringer.start()
+
 @sio.event
 def connect():
-    global currently_ringing
+    global currently_ringing, connected, _offline_ringer
 
     print('connection established')
     sio.emit("isPi", True)
+    connected = True
+
+    if _offline_ringer is not None:
+        _offline_ringer.terminate()
+        _offline_ringer.join()
 
     if currently_ringing is not None:
         sio.emit("ring-bell", currently_ringing)
 
+@sio.on("pi-init")
+def onPiInit(data):
+    global isLoop, speeds, patterns, today_times, last_date
+
+    patterns = data.get("patterns", patterns)
+    isLoop = data.get("isLoop", isLoop)
+    speeds = data.get("speeds", speeds)
+    last_date = data.get("date", last_date)
+    today_times = data.get("times", today_times)
+
+@sio.on("set-times-pi")
+def onSetTimesPi(data):
+    global today_times, last_date
+
+    last_date = data.get("date", last_date)
+    today_times = data.get("times", today_times)
+
 @sio.on("bell-rung")
 def onBellRung(bell):
-    global current_process
-    global symbol_times
-    global currently_ringing
+    global current_process, currently_ringing, patterns
 
-    if(currently_ringing is not None and bell is not None and currently_ringing.get("type") == bell.get("type", None)):
+    if bell is not None:
+        bell_type = bell.get("type", None)
+
+    if(currently_ringing is not None and bell is not None and currently_ringing.get("type") == bell_type):
         return
 
     currently_ringing = bell
 
     if bell != None:
-        symbol_times = bell.get('speeds', {
-            ' ': 0.5,
-            '.': 0.5,
-            '-': 1,
-            '_': 2,
-        })
-
-        start_process(bell.get('pattern', ' '), bell.get('isLoop', False))
+        start_process(patterns.get(bell_type, " "), isLoop.get(bell_type, False))
     elif current_process is not None:
         current_process.terminate()
         current_process.join()
@@ -93,18 +172,19 @@ def onBellRung(bell):
 
 @sio.event
 def disconnect():
-    print('disconnected from server')
+    global connected
+
+    connected = False
+    print('disconnected from server, started offline ringer')
+
+    start_offline_ringer()
 
 GPIO.output(relay, GPIO.LOW)
-connected = False
 while not connected:
     try:
-        sio.connect("http://192.168.110.225:3000")
+        sio.connect("http://192.168.110.225:80")
         print("Socket established")
         connected = True
     except Exception as ex:
         print("Failed to establish initial connnection to server:", type(ex).__name__)
         time.sleep(2)
-sio.wait()
-
-GPIO.cleanup()
